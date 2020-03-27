7eb68b7cb3a6af1ca65c2376a55b18828af1a24c
Adam Szalowski <adamszalowski99@gmail.com>
2020-03-27 23:31:50 +0100

    Setup Gradle and Android Studio

---
+++ android/src/com/mygdx/panda3/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.panda3;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.panda3.Panda3;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Panda3(), config);
+	}
+}
